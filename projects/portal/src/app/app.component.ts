fc661568f3aed55edfd0e5083901e1934b7ea56f
sapphinebz <sapphinebz@gmail.com>
2023-08-08 20:46:03 +0700

    added loadManifest

--- projects/portal/src/app/app.component.ts
+++ projects/portal/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewContainerRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { from } from 'rxjs';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,17 @@ export class AppComponent {
   vc = inject(ViewContainerRef);
 
   addComponent() {
-    from(import('mfe1/Component')).subscribe((m) => {
+    // from(import('mfe1/Component')).subscribe((m) => {
+    //   this.vc.createComponent(m.AppComponent);
+    // });
+
+    from(
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'mfe1',
+        exposedModule: './Component',
+      })
+    ).subscribe((m) => {
       this.vc.createComponent(m.AppComponent);
     });
   }
--- projects/portal/webpack.config.js
+++ projects/portal/webpack.config.js
@@ -4,15 +4,31 @@ const {
 } = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-  remotes: {
-    mfe1: "http://localhost:4201/remoteEntry.js",
-  },
+  // remotes: {
+  //   mfe1: "http://localhost:4201/remoteEntry.js",
+  // },
 
   shared: {
-    ...shareAll({
+    // ...shareAll({
+    //   singleton: true,
+    //   strictVersion: true,
+    //   requiredVersion: "auto",
+    // }),
+    "@angular/core": {
       singleton: true,
       strictVersion: true,
-      requiredVersion: "auto",
-    }),
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: true,
+    },
+    "@angular/common/http": {
+      singleton: true,
+      strictVersion: true,
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: true,
+    },
   },
 });
